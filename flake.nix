98943e479a661d19d83f2d43eb2b434754b745ef
Satwik Kambham <satwik.kambham@gmail.com>
2024-07-01 13:17:31 +0530

    added nix flake devshell and envrc

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Rust devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShell.${system} = pkgs.mkShell {
+        packages = with pkgs; [
+          rustup
+          rust-analyzer
+        ];
+      };
+    };
+}
