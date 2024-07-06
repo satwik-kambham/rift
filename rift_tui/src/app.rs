8ef2e6ecc60834a0ab5f486c4bf016e7d68cbdda
Satwik Kambham <satwik.kambham@gmail.com>
2024-07-06 08:52:08 +0530

    more refactoring

--- rift_tui/src/app.rs
+++ rift_tui/src/app.rs
@@ -1,4 +1,4 @@
-use std::io::stdout;
+use std::io::{self, stdout};
 
 use ratatui::{
     backend::CrosstermBackend,
@@ -7,6 +7,7 @@ use ratatui::{
         terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
         ExecutableCommand,
     },
+    prelude::*,
     style::Stylize,
     widgets::{Paragraph, Widget},
     Frame, Terminal,
@@ -15,12 +16,20 @@ use ratatui::{
 /// Type for ratatui terminal with crossterm backend
 pub type Tui = Terminal<CrosstermBackend<std::io::Stdout>>;
 
+/// State for each open buffer
+pub struct EditorState {
+    pub buffer: rift_explorer::buffer::line_buffer::LineTextBuffer,
+    pub selection: rift_explorer::buffer::line_buffer::Selection,
+    pub scroll_x: usize,
+    pub scroll_y: usize,
+}
+
 /// Text Editor with State using ratatui for TUI
 pub struct Editor {
     /// Exit flag
     pub exit: bool,
     /// Text buffers
-    pub buffers: Vec<rift_explorer::buffer::line_buffer::LineTextBuffer>,
+    pub buffers: Vec<EditorState>,
 }
 
 /// Initialize the TUI
@@ -54,14 +63,7 @@ impl Editor {
             terminal.draw(|frame| {
                 self.render_frame(frame);
             })?;
-
-            if event::poll(std::time::Duration::from_millis(16))? {
-                if let event::Event::Key(key) = event::read()? {
-                    if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                        self.exit = true;
-                    }
-                }
-            }
+            self.handle_events()?;
         }
         Ok(())
     }
@@ -70,10 +72,27 @@ impl Editor {
     fn render_frame(&self, frame: &mut Frame) {
         frame.render_widget(self, frame.size());
     }
+
+    /// Handle events
+    fn handle_events(&mut self) -> std::io::Result<()> {
+        if event::poll(std::time::Duration::from_millis(16))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    self.exit = true;
+                }
+            }
+        }
+        Ok(())
+    }
 }
 
 impl Widget for &Editor {
-    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer) {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        for x in area.left() + 5..area.right() - 5 {
+            for y in area.top() + 5..area.bottom() - 5 {
+                buf.get_mut(x, y).set_bg(Color::DarkGray);
+            }
+        }
         Paragraph::new("sdfsdf".green()).render(area, buf);
     }
 }
