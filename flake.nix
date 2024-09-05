b938166a383b00c647bf0e1ad27f3201d8c2782b
Satwik Kambham <satwik.kambham@gmail.com>
2024-09-05 22:12:23 +0530

    Added tauri app

--- flake.nix
+++ flake.nix
@@ -14,13 +14,46 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        libraries = with pkgs;[
+          webkitgtk
+          gtk3
+          cairo
+          gdk-pixbuf
+          glib
+          dbus
+          openssl_3
+          librsvg
+        ];
+
+        packages = with pkgs; [
+          curl
+          wget
+          pkg-config
+          dbus
+          openssl_3
+          glib
+          gtk3
+          libsoup
+          webkitgtk
+          librsvg
+
+          rust-bin.stable.latest.default
+          rust-analyzer
+          nodejs_20
+          cargo-tauri
+        ];
       in
       {
         devShells.default = with pkgs; mkShell {
-          buildInputs = with pkgs; [
-            rust-bin.stable.latest.default
-            rust-analyzer
-          ];
+          buildInputs = packages;
+
+          shellHook =
+          ''
+            # export WEBKIT_DISABLE_COMPOSITING_MODE=1
+            export WEBKIT_DISABLE_DMABUF_RENDERER=1
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+            export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+          '';
         };
       }
     );
