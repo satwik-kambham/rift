b7f0d6f4572637536d957f5e6f03120e10094a1a
Satwik Kambham <satwik.kambham@gmail.com>
2024-12-13 16:56:18 +0530

    added modal and key mapping

--- rift_tui/src/app.rs
+++ rift_tui/src/app.rs
@@ -1,22 +1,26 @@
 use std::time::Duration;
 
 use ratatui::{
-    crossterm::event::{self, KeyCode, KeyEventKind},
+    crossterm::event::{self, KeyCode, KeyEventKind, KeyModifiers},
     layout::{Constraint, Direction, Layout, Rect},
     style::Stylize,
     widgets::{self},
     DefaultTerminal,
 };
 use rift_core::{
+    actions::{perform_action, Action},
+    buffer::line_buffer::LineBuffer,
+    io::file_io,
     lsp::client::{start_lsp, LSPClientHandle},
     preferences::Preferences,
-    state::EditorState,
+    state::{EditorState, Mode},
 };
 
 pub struct App {
     pub state: EditorState,
     pub preferences: Preferences,
     pub lsp_handle: LSPClientHandle,
+    pub modal_list_state: widgets::ListState,
 }
 
 impl App {
@@ -26,6 +30,7 @@ impl App {
             state: EditorState::default(),
             preferences: Preferences::default(),
             lsp_handle,
+            modal_list_state: widgets::ListState::default(),
         }
     }
 
@@ -114,30 +119,183 @@ impl App {
                 frame.render_widget(greeting, h_layout[1]);
 
                 // Render Modal
-                let popup_area = Rect {
-                    x: 4,
-                    y: 2,
-                    width: frame.area().width - 8,
-                    height: frame.area().height - 4,
-                };
-                let modal_block = widgets::Block::default().borders(widgets::Borders::ALL);
-                let modal_layout = Layout::vertical([
-                    Constraint::Length(1),
-                    Constraint::Length(1),
-                    Constraint::Min(1),
-                ])
-                .split(modal_block.inner(popup_area));
-                frame.render_widget(widgets::Clear, popup_area);
-                frame.render_widget(modal_block, popup_area);
-                frame.render_widget("Hello", modal_layout[0]);
-                frame.render_widget("Hello World", modal_layout[2]);
+                if self.state.modal_open {
+                    let popup_area = Rect {
+                        x: 4,
+                        y: 2,
+                        width: frame.area().width - 8,
+                        height: frame.area().height - 4,
+                    };
+                    let modal_block = widgets::Block::default().borders(widgets::Borders::ALL);
+                    let modal_layout = Layout::vertical([
+                        Constraint::Length(1),
+                        Constraint::Length(1),
+                        Constraint::Min(1),
+                    ])
+                    .split(modal_block.inner(popup_area));
+                    let modal_list = self
+                        .state
+                        .modal_options_filtered
+                        .iter()
+                        .map(|entry| entry.name.clone())
+                        .collect::<widgets::List>();
+                    frame.render_widget(widgets::Clear, popup_area);
+                    frame.render_widget(modal_block, popup_area);
+                    frame.render_widget(&self.state.modal_input, modal_layout[0]);
+                    frame.render_stateful_widget(
+                        modal_list,
+                        modal_layout[2],
+                        &mut self.modal_list_state,
+                    );
+                }
             })?;
 
             // Handle keyboard events
             if event::poll(Duration::from_millis(5))? {
                 if let event::Event::Key(key) = event::read()? {
-                    if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                        return Ok(());
+                    if key.kind == KeyEventKind::Press {
+                        if self.state.modal_open {
+                            if let KeyCode::Char(char) = key.code {
+                                self.state.modal_input.push(char);
+                                self.state.modal_options_filtered = self
+                                    .state
+                                    .modal_options
+                                    .iter()
+                                    .filter(|entry| entry.path.starts_with(&self.state.modal_input))
+                                    .cloned()
+                                    .collect();
+                            } else if key.code == KeyCode::Tab {
+                                if !self.state.modal_options_filtered.is_empty() {
+                                    if self.state.modal_selection_idx.is_none() {
+                                        self.state.modal_selection_idx = Some(0);
+                                        self.modal_list_state.select(Some(0));
+                                    } else {
+                                        self.state.modal_selection_idx =
+                                            Some(self.state.modal_selection_idx.unwrap() + 1);
+                                        self.modal_list_state.select(Some(
+                                            self.state.modal_selection_idx.unwrap() + 1,
+                                        ));
+                                        if self.state.modal_selection_idx.unwrap()
+                                            >= self.state.modal_options_filtered.len()
+                                        {
+                                            self.state.modal_selection_idx = Some(0);
+                                            self.modal_list_state.select(Some(0));
+                                        }
+                                    }
+
+                                    self.state.modal_input = self.state.modal_options_filtered
+                                        [self.state.modal_selection_idx.unwrap()]
+                                    .path
+                                    .clone();
+                                } else {
+                                    self.state.modal_selection_idx = None;
+                                    self.modal_list_state.select(None);
+                                }
+                            } else if key.code == KeyCode::Backspace {
+                                self.state.modal_input.pop();
+                                self.state.modal_options_filtered = self
+                                    .state
+                                    .modal_options
+                                    .iter()
+                                    .filter(|entry| entry.path.starts_with(&self.state.modal_input))
+                                    .cloned()
+                                    .collect();
+                            } else if key.code == KeyCode::Enter {
+                                if self.state.modal_selection_idx.is_some() {
+                                    let entry = &self.state.modal_options_filtered
+                                        [self.state.modal_selection_idx.unwrap()];
+                                    if !entry.is_dir {
+                                        let path = entry.path.clone();
+                                        let initial_text =
+                                            file_io::read_file_content(&path).unwrap();
+                                        let buffer = LineBuffer::new(
+                                            initial_text.clone(),
+                                            Some(path.clone()),
+                                        );
+                                        self.state.buffer_idx = Some(self.state.add_buffer(buffer));
+                                        self.state.modal_open = false;
+                                        self.state.modal_options = vec![];
+                                        self.state.modal_options_filtered = vec![];
+                                        self.state.modal_selection_idx = None;
+                                        self.modal_list_state.select(None);
+                                        self.state.modal_input = "".into();
+
+                                        self.lsp_handle
+                                            .send_notification(
+                                                "textDocument/didOpen".to_string(),
+                                                Some(LSPClientHandle::did_open_text_document(
+                                                    path.clone(),
+                                                    initial_text,
+                                                )),
+                                            )
+                                            .await
+                                            .unwrap();
+                                    } else {
+                                        self.state.modal_input = entry.path.clone();
+
+                                        if key.modifiers == KeyModifiers::SHIFT {
+                                            self.state.workspace_folder = entry.path.clone();
+                                            self.lsp_handle
+                                                .init_lsp(self.state.workspace_folder.clone())
+                                                .await;
+                                        }
+
+                                        #[cfg(target_os = "windows")]
+                                        {
+                                            self.state.modal_input.push('\\');
+                                        }
+
+                                        #[cfg(any(target_os = "linux", target_os = "macos"))]
+                                        {
+                                            self.state.modal_input.push('/');
+                                        }
+
+                                        self.state.modal_options =
+                                            file_io::get_directory_entries(&entry.path).unwrap();
+                                        self.state.modal_options_filtered =
+                                            self.state.modal_options.clone();
+                                        self.state.modal_selection_idx = None;
+                                        self.modal_list_state.select(None);
+                                    }
+                                }
+                            } else if key.code == KeyCode::Esc {
+                                self.state.modal_open = false;
+                                self.state.modal_options = vec![];
+                                self.state.modal_options_filtered = vec![];
+                                self.state.modal_selection_idx = None;
+                                self.modal_list_state.select(None);
+                                self.state.modal_input = "".into();
+                            }
+                        } else if matches!(self.state.mode, Mode::Normal) {
+                            if key.code == KeyCode::Char('q') {
+                                return Ok(());
+                            } else if key.code == KeyCode::Char('i') {
+                                perform_action(
+                                    Action::EnterInsertMode,
+                                    &mut self.state,
+                                    &mut self.preferences,
+                                    &mut self.lsp_handle,
+                                );
+                            } else if key.code == KeyCode::Char('f') {
+                                perform_action(
+                                    Action::OpenFile,
+                                    &mut self.state,
+                                    &mut self.preferences,
+                                    &mut self.lsp_handle,
+                                );
+                            }
+                        } else if matches!(self.state.mode, Mode::Insert) {
+                            if key.code == KeyCode::Esc {
+                                perform_action(
+                                    Action::QuitInsertMode,
+                                    &mut self.state,
+                                    &mut self.preferences,
+                                    &mut self.lsp_handle,
+                                );
+                            } else {
+                                println!("{:#?}", key.code);
+                            }
+                        }
                     }
                 }
             }
