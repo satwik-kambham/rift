349a45666708c0cc7c6f7311b884dfee56f251f4
Satwik Kambham <satwik.kambham@gmail.com>
2025-01-29 14:18:51 +0000

    added naersk for building packages

--- flake.nix
+++ flake.nix
@@ -3,16 +3,23 @@
 
   inputs = {
     nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, naersk, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
+        };
+        toolchain = pkgs.rust-bin.stable.latest.default;
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+          clippy = toolchain;
         };
         libraries = with pkgs;[
           rust-bin.stable.latest.default
@@ -39,6 +46,21 @@
         ];
       in
       {
+        packages.rift_egui = naersk'.buildPackage {
+          pname = "rift_egui";
+          src = ./.;
+          cargoBuildOptions = x: x ++ [ "--package rift_egui" ];
+          nativeBuildInputs = with pkgs; [
+            fontconfig
+          ];
+        };
+
+        packages.rift_tui = naersk'.buildPackage {
+          pname = "rift_tui";
+          src = ./.;
+          cargoBuildOptions = x: x ++ [ "--package rift_tui" ];
+        };
+
         devShells.default = with pkgs; mkShell {
           buildInputs = libraries;
 
