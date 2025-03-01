98c49adf04ae3ae0befd3879c014970b0d67e205
Satwik Kambham <satwik.kambham@gmail.com>
2025-03-01 11:33:55 +0530

    update flake runtime deps

--- flake.nix
+++ flake.nix
@@ -21,11 +21,12 @@
           rustc = toolchain;
           clippy = toolchain;
         };
-        libraries = with pkgs;[
+        buildDeps = with pkgs; [
           rust-bin.stable.latest.default
           rust-analyzer
           trunk
-
+        ];
+        runtimeDeps = with pkgs; [
           # misc. libraries
           openssl
           pkg-config
@@ -43,11 +44,13 @@
           xorg.libXrandr
           xorg.libXi
           xorg.libX11
-
+        ];
+        appDeps = with pkgs; [
           fzf
           ripgrep
           fd
         ];
+        devDeps = buildDeps ++ runtimeDeps ++ appDeps;
       in
       {
         packages.rift_egui = naersk'.buildPackage {
@@ -56,21 +59,34 @@
           cargoBuildOptions = x: x ++ [ "--package rift_egui" ];
           nativeBuildInputs = with pkgs; [
             fontconfig
+            makeWrapper
           ];
+          postInstall = ''
+            wrapProgram $out/bin/rift_egui \
+              --prefix PATH : ${pkgs.lib.makeBinPath appDeps} \
+              --set LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath runtimeDeps}
+          '';
         };
 
         packages.rift_tui = naersk'.buildPackage {
           pname = "rift_tui";
           src = ./.;
           cargoBuildOptions = x: x ++ [ "--package rift_tui" ];
+          nativeBuildInputs = with pkgs; [
+            makeWrapper
+          ];
+          postInstall = ''
+            wrapProgram $out/bin/rift_tui \
+              --prefix PATH : ${pkgs.lib.makeBinPath appDeps}
+          '';
         };
 
         devShells.default = with pkgs; mkShell {
-          buildInputs = libraries;
+          buildInputs = devDeps;
 
           shellHook =
           ''
-            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath devDeps}:$LD_LIBRARY_PATH
           '';
         };
       }
