f00befb88da92320fc450a3b7242ddbcd573569a
Satwik Kambham <satwik.kambham@gmail.com>
2025-05-17 12:59:51 +0530

    added ws handler

--- crates/rift_ai/src/main.rs
+++ crates/rift_ai/src/main.rs
@@ -3,10 +3,14 @@ use std::sync::{Arc, Mutex};
 
 use axum::{
     Router,
-    extract::{DefaultBodyLimit, State},
-    routing::{get, post},
+    extract::{
+        DefaultBodyLimit, State,
+        ws::{Message, WebSocket, WebSocketUpgrade},
+    },
+    response::IntoResponse,
+    routing::{any, get, post},
 };
-use futures_util::StreamExt;
+use futures_util::{SinkExt, StreamExt};
 
 #[derive(serde::Serialize, serde::Deserialize, Clone)]
 pub struct OllamaChatMessage {
@@ -41,6 +45,7 @@ async fn main() {
         .route("/", get(info))
         .route("/transcribe", post(transcribe))
         .route("/chat", post(ollama_chat))
+        .route("/ws", any(websocket_handler))
         .with_state(server_state)
         .layer(DefaultBodyLimit::disable());
 
@@ -66,12 +71,59 @@ async fn transcribe(
         let data = field.bytes().await.unwrap();
         file.write_all(&data).unwrap();
 
-        let transcript = String::new();
+        let client = reqwest::Client::new();
+        let form = reqwest::multipart::Form::new()
+            .file("file", file_path)
+            .await
+            .unwrap();
+        let response = client
+            .post("http://localhost:5000/transcribe")
+            .multipart(form)
+            .send()
+            .await
+            .unwrap();
+
+        let transcript = response.text().await.unwrap();
 
         return transcript;
     }
 
     String::new()
+}
+
+async fn websocket_handler(
+    ws: WebSocketUpgrade,
+    State(state): State<Arc<Mutex<ServerState>>>,
+) -> impl IntoResponse {
+    ws.on_upgrade(|socket| handle_socket(socket, state))
+}
+
+async fn handle_socket(stream: WebSocket, state: Arc<Mutex<ServerState>>) {
+    let (mut sender, mut receiver) = stream.split();
+
+    tokio::spawn(async move {
+        while let Some(Ok(message)) = &receiver.next().await {
+            if let Message::Text(text) = message {
+                let text = text.as_str();
+
+                let body = serde_json::json!({
+                    "text": text,
+                });
+                let client = reqwest::Client::new();
+                let response = client
+                    .post("http://localhost:5000/tts")
+                    .json(&body)
+                    .send()
+                    .await
+                    .unwrap();
+                let tts = response.bytes().await.unwrap();
+
+                sender.send(Message::binary(tts)).await.unwrap();
+            }
+        }
+    });
+
+    state.lock().unwrap().recordings_path = "".into();
 }
 
 async fn ollama_chat(State(state): State<Arc<Mutex<ServerState>>>) {
