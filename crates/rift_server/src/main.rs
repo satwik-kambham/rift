85bf9b0b6866a3a4c6fec61af83e6c8dfa9de055
Satwik Kambham <satwik.kambham@gmail.com>
2025-11-13 11:39:56 +0530

    logging filtering

--- crates/rift_server/src/main.rs
+++ crates/rift_server/src/main.rs
@@ -10,6 +10,7 @@ fn main() -> anyhow::Result<()> {
         tmp_dir.push("rift_logs");
         let file_appender = tracing_appender::rolling::never(tmp_dir, "rift.log");
         tracing_subscriber::fmt()
+            .with_env_filter("info.tarpc=off")
             .with_writer(file_appender)
             .with_ansi(false)
             .with_level(true)
