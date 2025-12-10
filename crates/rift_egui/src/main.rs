541702c43b26524172ce8b0d29e29fe1fe398317
Satwik Kambham <satwik.kambham@gmail.com>
2025-12-10 14:53:43 +0530

    chore: enable tracing file logging in release binaries

--- crates/rift_egui/src/main.rs
+++ crates/rift_egui/src/main.rs
@@ -10,18 +10,16 @@ pub mod components;
 pub mod fonts;
 
 fn main() -> eframe::Result {
-    #[cfg(debug_assertions)]
-    {
-        let mut tmp_dir = std::env::temp_dir();
-        tmp_dir.push("rift_logs");
-        let file_appender = tracing_appender::rolling::never(tmp_dir, "rift.log");
-        tracing_subscriber::fmt()
-            .with_env_filter("debug,tarpc=error")
-            .with_writer(file_appender)
-            .with_ansi(false)
-            .with_level(true)
-            .init();
-    }
+    let mut tmp_dir = std::env::temp_dir();
+    tmp_dir.push("rift_logs");
+    let file_appender = tracing_appender::rolling::never(tmp_dir, "rift.log");
+    tracing_subscriber::fmt()
+        .with_env_filter("debug,tarpc=error")
+        .with_writer(file_appender)
+        .with_ansi(false)
+        .with_level(true)
+        .init();
+
     let cli_args = CLIArgs::parse();
     let native_options = eframe::NativeOptions::default();
     let rt = tokio::runtime::Builder::new_multi_thread()
--- crates/rift_tui/src/main.rs
+++ crates/rift_tui/src/main.rs
@@ -4,18 +4,16 @@ use rift_core::cli::CLIArgs;
 pub mod app;
 
 fn main() -> anyhow::Result<()> {
-    #[cfg(debug_assertions)]
-    {
-        let mut tmp_dir = std::env::temp_dir();
-        tmp_dir.push("rift_logs");
-        let file_appender = tracing_appender::rolling::never(tmp_dir, "rift.log");
-        tracing_subscriber::fmt()
-            .with_env_filter("debug,tarpc=error")
-            .with_writer(file_appender)
-            .with_ansi(false)
-            .with_level(true)
-            .init();
-    }
+    let mut tmp_dir = std::env::temp_dir();
+    tmp_dir.push("rift_logs");
+    let file_appender = tracing_appender::rolling::never(tmp_dir, "rift.log");
+    tracing_subscriber::fmt()
+        .with_env_filter("debug,tarpc=error")
+        .with_writer(file_appender)
+        .with_ansi(false)
+        .with_level(true)
+        .init();
+
     let cli_args = CLIArgs::parse();
     let mut terminal = ratatui::init();
     terminal.clear()?;
