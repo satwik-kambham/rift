17440d2bd3857a74aaac81123cfa0fc19be880ef
Satwik Kambham <satwik.kambham@gmail.com>
2026-05-08 16:33:24 +0530

    added tracing

--- crates/rift_tui/src/main.rs
+++ crates/rift_tui/src/main.rs
@@ -1,7 +1,16 @@
+use rift_io::logging::initialize_tracing;
+
 pub mod app;
 
 fn main() -> anyhow::Result<()> {
+    initialize_tracing();
+
+    tracing::info!("Rift session starting (tui)");
+
     let mut tui_app = app::App::new();
     tui_app.run();
+
+    tracing::info!("Rift session exiting (tui)");
+
     Ok(())
 }
