fabd4e8f1e7000c1d7f0a7f7cc1605098123dd7d
Satwik Kambham <satwik.kambham@gmail.com>
2026-05-08 15:26:20 +0530

    added tokio

--- crates/rift_core/src/state.rs
+++ crates/rift_core/src/state.rs
@@ -1,12 +1,14 @@
 use crate::compositor::Compositor;
 
 pub struct EditorState {
-    compositor: Compositor,
+    pub quit: bool,
+    pub compositor: Compositor,
 }
 
 impl EditorState {
     pub fn new() -> Self {
         Self {
+            quit: false,
             compositor: Compositor::new(),
         }
     }
--- crates/rift_tui/src/app.rs
+++ crates/rift_tui/src/app.rs
@@ -1,8 +1,14 @@
-pub struct App {}
+use rift_core::state::EditorState;
+
+pub struct App {
+    state: EditorState,
+}
 
 impl App {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            state: EditorState::new(),
+        }
     }
 
     pub fn run(&mut self) {}
